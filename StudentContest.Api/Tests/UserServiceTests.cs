77e98ab6de22a6ce74fd61500769ae4e9300581f
Anna Baulina <anna.baulina@forsta.com>
2022-02-17 16:13:02 +0300

    Add tests

--- StudentContest.Api/Tests/UserServiceTests.cs
+++ StudentContest.Api/Tests/UserServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Authentication;
 using System.Threading.Tasks;
@@ -14,7 +15,6 @@ namespace Tests
 {
     public class UserServiceTests
     {
-        private UserService _userService;
         private readonly UserContext _context;
 
         public UserServiceTests()
@@ -25,11 +25,11 @@ namespace Tests
         [Fact]
         public async Task Login_InvalidEmail_ThrowsException()
         {
-            _userService = new UserService(_context, new Mock<IJwtUtils>().Object,
+            var userService = new UserService(_context, new Mock<IJwtUtils>().Object,
                 new Mock<IRegisterRequestValidator>().Object, new Mock<IPasswordHasher>().Object);
             var loginRequest = new LoginRequest {Email = "new@example.com", Password = "12345678"};
 
-            await Assert.ThrowsAsync<InvalidCredentialException>(() => _userService.Login(loginRequest, "ipAddress"));
+            await Assert.ThrowsAsync<InvalidCredentialException>(() => userService.Login(loginRequest, "ipAddress"));
         }
 
         [Fact]
@@ -38,11 +38,11 @@ namespace Tests
            var bCryptFake = new Mock<IPasswordHasher>();
             bCryptFake.Setup(x => x.VerifyPassword(It.IsAny<string>(), It.IsAny<string>()))
                 .Returns((string password, string hash) => password == hash);
-            _userService = new UserService(_context, new Mock<IJwtUtils>().Object,
+            var userService = new UserService(_context, new Mock<IJwtUtils>().Object,
                 new Mock<IRegisterRequestValidator>().Object, bCryptFake.Object);
             var loginRequest = new LoginRequest { Email = "test@example.com", Password = "123456" };
 
-            await Assert.ThrowsAsync<InvalidCredentialException>(() => _userService.Login(loginRequest, "ipAddress"));
+            await Assert.ThrowsAsync<InvalidCredentialException>(() => userService.Login(loginRequest, "ipAddress"));
         }
 
         [Fact]
@@ -58,11 +58,11 @@ namespace Tests
             });
             jwtUtilsFake.Setup(x => x.GenerateToken(It.IsAny<User>())).Returns("token");
             jwtUtilsFake.Setup(x => x.RemoveOldRefreshTokens(It.IsAny<User>()));
-            _userService = new UserService(_context, jwtUtilsFake.Object,
+            var userService = new UserService(_context, jwtUtilsFake.Object,
                 new Mock<IRegisterRequestValidator>().Object, bCryptFake.Object);
             var loginRequest = new LoginRequest { Email = "test@example.com", Password = "12345678" };
 
-            var result = await _userService.Login(loginRequest, "ipAddress");
+            var result = await userService.Login(loginRequest, "ipAddress");
 
             Assert.IsType<AuthenticateResponse>(result);
             Assert.Equal("refreshToken", result.RefreshToken);
@@ -75,19 +75,19 @@ namespace Tests
         [Fact]
         public async Task Logout_InvalidToken_ThrowsException()
         {
-            _userService = new UserService(_context, new Mock<IJwtUtils>().Object,
+            var userService = new UserService(_context, new Mock<IJwtUtils>().Object,
                 new Mock<IRegisterRequestValidator>().Object, new Mock<IPasswordHasher>().Object);
 
-            await Assert.ThrowsAsync<ArgumentException>(() => _userService.Logout("", "ipAddress"));
+            await Assert.ThrowsAsync<ArgumentException>(() => userService.Logout("", "ipAddress"));
         }
 
         [Fact]
         public async Task Logout_AlreadyRevokedToken_ThrowsException()
         {
-            _userService = new UserService(_context, new Mock<IJwtUtils>().Object,
+            var userService = new UserService(_context, new Mock<IJwtUtils>().Object,
                 new Mock<IRegisterRequestValidator>().Object, new Mock<IPasswordHasher>().Object);
 
-            await Assert.ThrowsAsync<ArgumentException>(() => _userService.Logout("alreadyRevoked", "ipAddress"));
+            await Assert.ThrowsAsync<ArgumentException>(() => userService.Logout("alreadyRevoked", "ipAddress"));
         }
 
         [Fact]
@@ -97,14 +97,90 @@ namespace Tests
             jwtUtilsFake
                 .Setup(x => x.RevokeRefreshToken(It.IsAny<RefreshToken>(), It.IsAny<string>(), It.IsAny<string>(),
                     It.IsAny<string>())).Callback((RefreshToken token, string _, string _, string _) => token.Token = "justRevokedToken");
-            _userService = new UserService(_context, jwtUtilsFake.Object,
+            var userService = new UserService(_context, jwtUtilsFake.Object,
                 new Mock<IRegisterRequestValidator>().Object, new Mock<IPasswordHasher>().Object);
 
-            await _userService.Logout("notRevoked", "ipAddress");
+            await userService.Logout("notRevoked", "ipAddress");
 
             jwtUtilsFake.Verify(x=> x.RevokeRefreshToken(It.IsAny<RefreshToken>(), "ipAddress", It.IsAny<string>(),
                 It.IsAny<string>()), Times.Once);
             Assert.NotNull(_context.Users.FirstOrDefault(x => x.RefreshTokens.LastOrDefault().Token == "justRevokedToken"));
+        }
+
+        [Fact]
+        public async Task GetUser_NonExistingUser_ThrowsException()
+        {
+            var userService = new UserService(_context, new Mock<IJwtUtils>().Object,
+                new Mock<IRegisterRequestValidator>().Object, new Mock<IPasswordHasher>().Object);
+
+            await Assert.ThrowsAsync<KeyNotFoundException>(() => userService.GetCurrentUserInfo(-1));
+        }
+
+        [Fact]
+        public async Task GetUser_Success_ReturnsUser()
+        {
+            var userService = new UserService(_context, new Mock<IJwtUtils>().Object,
+                new Mock<IRegisterRequestValidator>().Object, new Mock<IPasswordHasher>().Object);
+
+           var result= await userService.GetCurrentUserInfo(1);
+
+           Assert.NotNull(result);
+           Assert.IsType<User>(result);
+           Assert.Equal(1, result.Id);
+        }
+
+        [Fact]
+        public async Task Register_Success_ChangesDatabase()
+        {
+            var bCryptFake = new Mock<IPasswordHasher>();
+            bCryptFake.Setup(x => x.HashPassword(It.IsAny<string>()))
+                .Returns((string password) => password);
+            var userService = new UserService(_context, new Mock<IJwtUtils>().Object,
+                new Mock<IRegisterRequestValidator>().Object, bCryptFake.Object);
+            var registerRequest = new RegisterRequest
+            {
+                Email = "newUser@example.com", FirstName = "New", LastName = "User",
+                Password = "12345678"
+            };
+            var count = _context.Users.Count();
+
+            await userService.Register(registerRequest);
+
+            Assert.Equal(count+1, _context.Users.Count());
+            Assert.Equal("newUser@example.com",_context.Users.Last().Email);
+        }
+
+        [Fact]
+        public async Task RefreshToken_UnknownToken_ThrowsException()
+        {
+            var userService = new UserService(_context, new Mock<IJwtUtils>().Object,
+                new Mock<IRegisterRequestValidator>().Object, new Mock<IPasswordHasher>().Object);
+
+            await Assert.ThrowsAsync<ArgumentException>(() => userService.RefreshToken("unknown", "ipAddress"));
+        }
+
+        [Fact]
+        public async Task RefreshToken_NotActiveToken_ThrowsException()
+        {
+            var userService = new UserService(_context, new Mock<IJwtUtils>().Object,
+                new Mock<IRegisterRequestValidator>().Object, new Mock<IPasswordHasher>().Object);
+
+            await Assert.ThrowsAsync<ArgumentException>(() => userService.RefreshToken("alreadyRevoked", "ipAddress"));
+        }
+
+        [Fact]
+        public async Task RefreshToken_Success_ChangesDatabase()
+        {
+            var jwtUtilsFake = new Mock<IJwtUtils>();
+            jwtUtilsFake
+                .Setup(x => x.RotateRefreshToken(It.IsAny<RefreshToken>(), It.IsAny<string>()))
+                .Returns(new RefreshToken {Token = "rotatedToken"});
+            var userService = new UserService(_context, jwtUtilsFake.Object,
+                new Mock<IRegisterRequestValidator>().Object, new Mock<IPasswordHasher>().Object);
+
+            await userService.RefreshToken("notRevoked", "ipAddress");
+
+            Assert.Equal("rotatedToken", _context.Users.Last().RefreshTokens.Last().Token);
         }
     }
 }
